e5f38ecf906f948ff05dd1710e69c4f1daa13d6b
René Hansen <rene@hansen-audio.de>
2021-09-04 23:02:06 +0200

    Adding a Debug formatting for Component trait

--- src/component.rs
+++ src/component.rs
@@ -1,9 +1,20 @@
 // Copyright(c) 2021 René Hansen.
 // Distributed under the MIT License (http://opensource.org/licenses/MIT)
 
+use std::fmt;
+
 use crate::{IDType, Visitor};
 
 pub trait Component {
     fn get_uid(&self) -> IDType;
     fn accept(&self, visitor: &mut dyn Visitor);
+}
+
+// e.g. println!("{:?}", component); prints "Component { uid: 1 }"
+impl fmt::Debug for dyn Component {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Component")
+            .field("uid", &self.get_uid())
+            .finish()
+    }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -7,12 +7,20 @@ mod tests {
     use multimap::MultiMap;
 
     #[test]
-    fn from_builder() {
+    fn test_from_builder() {
         const TREE_MAP_DEF: &[(IDType, IDType)] = &[(1, 2), (1, 3), (1, 4), (4, 5), (4, 6), (1, 7)];
         let tree_map: MultiMap<IDType, IDType> = TREE_MAP_DEF.iter().cloned().collect();
 
         let root = builder::build_composite(1, &tree_map);
-
         root.accept(&mut Printer::new());
+    }
+
+    #[test]
+    fn test_component_debug_print() {
+        const TREE_MAP_DEF: &[(IDType, IDType)] = &[(1, 2), (1, 3), (1, 4), (4, 5), (4, 6), (1, 7)];
+        let tree_map: MultiMap<IDType, IDType> = TREE_MAP_DEF.iter().cloned().collect();
+
+        let root = builder::build_composite(1, &tree_map);
+        println!("{:?}", &root);
     }
 }
